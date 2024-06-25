454a8236432c1a43669b57d025d829357f688075
Octavio Paz <octavioemanuelpaz@gmail.com>
2024-06-25 14:14:57 -0300

    Update ResultadoGotero.jsx
    correccion a español

--- frontend/src/components/gotero/ResultadoGotero.jsx
+++ frontend/src/components/gotero/ResultadoGotero.jsx
@@ -2,11 +2,23 @@ import React from 'react';
 import './Gotero.css'; // Asegúrate de importar el archivo CSS
 
 const ResultadoGotero = ({ results, dropCount }) => {
+  const volumeUnitMap = {
+    liters: 'litros',
+    milliliters: 'mililitros',
+  };
+
+  const timeUnitMap = {
+    hours: 'horas',
+    minutes: 'minutos',
+  };
+
+  const volumeUnit = volumeUnitMap[results.volumeUnit] || results.volumeUnit;
+  const timeUnit = timeUnitMap[results.timeUnit] || results.timeUnit;
   return (
     <div className="resultado-container">
       <p className="tipo-gotero">Tipo de Gotero: {results.goteroType}</p>
-      <p>Volumen: {results.volume} {results.volumeUnit}</p>
-      <p>Tiempo: {results.time} {results.timeUnit}</p>
+      <p>Volumen: {results.volume} {volumeUnit}</p>
+      <p>Tiempo: {results.time} {timeUnit}</p>
       <p>Gotas por minuto: {results.dropsPerMinute}</p>
       <p>Gotas cayendo: {dropCount}</p>
     </div>
